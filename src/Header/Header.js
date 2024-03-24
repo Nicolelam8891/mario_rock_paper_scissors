de78d57104c2632298e9ed54f4346664251658b5
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2024-03-24 12:00:21 -0600

    Get tailwind to work on header

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  return (
+    <main className='header'>
+      <h1 className='text-3xl font-bold text-center sm:text-2xl'>Mushroom Kingdom Showdown</h1>
+      <p className='game-title'>Choose your game mode!</p>
+      
+    </main>
+  )
+}
+
+export default Header;
