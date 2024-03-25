97721b8468968945273586cdf9ea7e7015e95e5e
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2024-03-24 18:43:53 -0600

    Adjust title styling

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,8 +5,8 @@ const Header = () => {
       <div className="flex justify-content p-1">
         <img src="/toad.png" className="w-60 h-auto mx-auto p-2" alt= "Toad family" />
       </div>
-        <h1 className='text-3xl font-bold text-center'>Mushroom Kingdom Showdown</h1>
-        <p className='text-2xl font-bold text-center'>Choose your game mode!</p>
+        <h1 className='text-6xl font-bold text-center'>Mushroom Kingdom Showdown</h1>
+        <p className='text-4xl font-bold text-center'>Choose your game mode!</p>
     </main>
   )
 }
