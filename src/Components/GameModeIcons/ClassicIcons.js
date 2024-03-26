320b3fe81e81531b906ba20191a9c8112597f6eb
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2024-03-26 16:27:59 -0600

    Add styling to both classic and difficult icons

--- src/Components/GameModeIcons/ClassicIcons.js
+++ src/Components/GameModeIcons/ClassicIcons.js
@@ -5,7 +5,7 @@ import starIcon from '../../assets/star.png'
 
 const ClassicIcons = () => {
   return (
-    <div className='icon-container flex justify-center items-center space-x-4'>
+    <div className='icon-container flex w-60 justify-center items-center space-x-20'>
       <img src={bombIcon} alt="Bomb icon" />
       <img src={shellIcon} alt="Shell icon" />
       <img src={starIcon} alt="Star icon" />
