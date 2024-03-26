6c40d08dac8873c74aeaa8fac979aed7b421ba67
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2024-03-26 09:55:53 -0600

    Fix styling for boxes, opacity

--- src/Components/GameBoard/GameBoard.js
+++ src/Components/GameBoard/GameBoard.js
@@ -2,29 +2,47 @@ import './GameBoard.css'
 
 const GameBoard = () => {
 
+  const determineWinner = (playerChoice, computerChoice) => {
+    const rules = {
+      bomb: ['shell', 'daisy fireball'], 
+      shell: ['star', 'coin'], 
+      star: ['bomb', 'daisy fireball'],
+      coin: ['shell', 'star'],
+      daisyFireball: ['coin', 'bomb']
+    }
+    if (playerChoice === computerChoice) {
+      return `It's a draw!`
+    }
+
+    if (rules[playerChoice].includes(computerChoice)) {
+      return `Good work! Peach wins!`
+    } else {
+      return `Bowser wins!`;
+    }
+  }
 
   return (
     <main className='flex justify-center items-center h-screen'>
       <div className='flex flex-col items-center justify-center p-4 h-full'>
-        <button className='bg-blue-100 p-10 border-2 classic'>
+        <button className='bg-blue-100 p-10 border-2 classic bg-opacity-50'>
           <div className='text-left'>
-            <p className='text-center'>Classic Game:</p>
+            <p className='text-center font-bold'>Classic Mode:</p>
             <img src='/classicGame.png' className='w-40' />
-            <p className='m1-4 text-center'>Bomb beats Shell</p>
-            <p className='m1-4 text-center'>Shell beats Star</p>
-            <p className='m1-4 text-center'>Star beats Bomb</p>
+            <p className='m1-4 text-center font-bold'>Bomb beats Shell</p>
+            <p className='m1-4 text-center font-bold'>Shell beats Star</p>
+            <p className='m1-4 text-center font-bold'>Star beats Bomb</p>
           </div>
         </button>
-          
-        <button className='bg-blue-100 p-10 difficult'>
+        
+        <button className='bg-blue-100 p-10 bg-opacity-50'>
           <div className='text-left'>
-              <p className='text-center'>Classic Game:</p>
+              <p className='text-center font-bold'>Difficult Mode:</p>
               <img src='/difficultGame.png' className='w-40' />
-              <p className='m1-4 text-center'>Bomb beats Shell & Daisy Fireball</p>
-              <p className='m1-4 text-center'>Shell beats Star & Coin</p>
-              <p className='m1-4 text-center'>Star beats Bomb & Daisy Fireball</p>
-              <p className='m1-4 text-center'>Coin beats Shell and Star</p>
-              <p className='m1-4 text-center'>Daisy Fireball beats Coin and Bomb</p>
+              <p className='m1-4 text-center font-bold'>Bomb beats Shell & Daisy Fireball</p>
+              <p className='m1-4 text-center font-bold'>Shell beats Star & Coin</p>
+              <p className='m1-4 text-center font-bold'>Star beats Bomb & Daisy Fireball</p>
+              <p className='m1-4 text-center font-bold'>Coin beats Shell and Star</p>
+              <p className='m1-4 text-center font-bold'>Daisy Fireball beats Coin and Bomb</p>
             </div>
         
         </button>
